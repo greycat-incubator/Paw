95f4029cc59199cf0cb3a36844f4e0294d7055c6
Matthieu Jimenez <matthieu@jimenez.lu>
2017-03-21 19:26:11 +0100

    tokenization tasks

---
+++ src/main/java/paw/greycat/tasks/TokenizationTasks.java
@@ -0,0 +1,95 @@
+package paw.greycat.tasks;
+
+import greycat.Task;
+import paw.tokeniser.Tokenizer;
+import paw.tokeniser.preprocessing.PreprocessorFactory;
+import paw.tokeniser.tokenisation.TokenizerFactory;
+import paw.tokeniser.tokenisation.pl.java.JavaTokenizer;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import static greycat.Tasks.newTask;
+import static mylittleplugin.MyLittleActions.injectAsVar;
+
+public class TokenizationTasks {
+
+    public static Task createTokenizer(String tokenizerVar, byte tokenizerType, boolean keepDelimiter) {
+        Tokenizer tokenizer = TokenizerFactory.getTokenizer(tokenizerType);
+        tokenizer.setKeepDelimiter(keepDelimiter);
+        return newTask().then(injectAsVar(tokenizerVar, tokenizer));
+    }
+
+    public static Task addPreprocessor(String tokenizerVar, byte... preprocessorType) {
+        return newTask()
+                .thenDo(ctx -> {
+                    Tokenizer tokenizer = (Tokenizer) ctx.variable(tokenizerVar).get(0);
+                    for (int i = 0; i < preprocessorType.length; i++) {
+                        tokenizer.addPreprocessor(PreprocessorFactory.getPreprocessor(preprocessorType[i]));
+                    }
+                    ctx.setVariable(tokenizerVar, tokenizer);
+                    ctx.continueTask();
+                });
+    }
+
+    public static Task setTypeOfToken(String tokenizerVar, String type) {
+        return newTask()
+                .thenDo(ctx -> {
+                    Tokenizer tokenizer = (Tokenizer) ctx.variable(tokenizerVar).get(0);
+                    tokenizer.setTypeOfToken(type);
+                    ctx.setVariable(tokenizerVar, tokenizer);
+                    ctx.continueTask();
+                });
+    }
+
+    public static Task setRemoveComment(String tokenizerVar, boolean removeComments) {
+        return newTask()
+                .thenDo(ctx -> {
+                    Tokenizer tokenizer = (Tokenizer) ctx.variable(tokenizerVar).get(0);
+                    if (tokenizer instanceof JavaTokenizer) {
+                        ((JavaTokenizer) tokenizer).setRemoveComments(removeComments);
+                        ctx.setVariable(tokenizerVar, tokenizer);
+                    }
+                    ctx.continueTask();
+
+                });
+    }
+
+    public static Task tokenizeFromString(String tokenizerVar, String... content) {
+        return newTask()
+                .then(injectAsVar("mycontents", content))
+                .pipe(tokenizeFromVar(tokenizerVar, "mycontents"));
+    }
+
+    public static Task tokenizeFromReader(String tokenizerVar, Reader... reader) {
+        return newTask()
+                .inject(reader)
+                .map(
+                        newTask()
+                                .thenDo(ctx -> {
+                                    Tokenizer tokenizer = (Tokenizer) ctx.variable(tokenizerVar).get(0);
+                                    Reader content = (Reader) ctx.result().get(0);
+                                    try {
+                                        String[] result = tokenizer.tokenize(content);
+                                        ctx.continueWith(ctx.wrap(result));
+                                    } catch (IOException e) {
+                                        ctx.endTask(ctx.result(), e);
+                                    }
+                                })
+                );
+    }
+
+    public static Task tokenizeFromVar(String tokenizerVar, String variable) {
+        return newTask()
+                .readVar(variable)
+                .map(
+                        newTask()
+                                .thenDo(ctx -> {
+                                    Tokenizer tokenizer = (Tokenizer) ctx.variable(tokenizerVar).get(0);
+                                    String content = ctx.resultAsStrings().get(0);
+                                    ctx.continueWith(ctx.wrap(tokenizer.tokenize(content)));
+                                })
+                );
+    }
+
+}
